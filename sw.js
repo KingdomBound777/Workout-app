f276ed1b8986c8650d6a43717de27c6fcae33f56
KingdomBound777 <buuaustinbess200@gmail.com>
2026-06-04 01:33:34 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "workout-calculator-v2";
+const CACHE_NAME = "workout-calculator-v3";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest",
+  "./styles.css?v=3",
+  "./app.js?v=3",
+  "./manifest.webmanifest?v=3",
 ];
 
 self.addEventListener("install", (event) => {
